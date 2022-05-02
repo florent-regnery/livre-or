767af9e4f1ce983be8a5bbf02472217e7f61c2d7
Florent Regnery <florent.regnery@laplateforme.io>
2022-05-02 21:28:37 +0200

    changement d'arbre

--- header.php
+++ header.php
@@ -5,13 +5,12 @@ if (session_status() === PHP_SESSION_NONE) {
 ?>
  <!-- on verif' si l'utilisateur est connecté dans ce cas on lui affiche des options différentes -->
 <?php if (isset($_SESSION['id'])) : ?>
-    <p>Vous êtes connecté</p>
-    <ul class="menu-link">
-        <li><a href="index.php" class="href">Accueil</a></li>
-        <li><a href="livre-or.php" class="href">Livre dor</a></li>
-        <li><a href="home_utilisateurs.php" class="inscription">Page Utilisateur</a></li>
-        <li><a href="deconnexion.php" class="connexion">Déconnexion</a></li>
-    </ul>
+    <p class="menu-link">
+        <a href="index.php" class="href">Accueil</a></li>
+        <a href="livre-or.php" class="href">Livre dor</a></li>
+        <a href="home_utilisateurs.php" class="inscription">Page Utilisateur</a></li>
+        <a href="deconnexion.php" class="connexion">Déconnexion</a></li>
+    </p>
 <?php else : ?>
     <a href="index.php" class="href">Accueil</a>
     <a href="livre-or.php" class="href">Livre d'or</a>
